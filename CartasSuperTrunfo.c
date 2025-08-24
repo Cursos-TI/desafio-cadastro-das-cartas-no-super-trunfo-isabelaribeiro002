d761114544f267c17678d937765a768fd85bf58e
Isabela Nicole Ribeiro <isabelaribeiro002@gmail.com>
2025-08-24 13:19:46 -0300

    Update Desafio: nível mestre CartasSuperTrunfo.c
    Desafio nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,23 +6,26 @@ int main() {
     char Estado1[50];
     char codigocarta1[4];
     char Nomecidade1[50];   
-    int populacao1;
+    unsigned long int populacao1; //guardar grande números sem ser negativo
     float area1;
     float pib1;
     int pontosTuristicos1;
     float densidadePopulacional1;
     float pibPerCapita1;
+    float superPoder1;
+
 
 // Carta 2
     char Estado2[50];
     char codigocarta2[4];
     char Nomecidade2[50];
-    int populacao2;
+    unsigned long int populacao2; //guardar grande números sem ser negativo
     float area2;
     float pib2;
     int pontosTuristicos2;
     float densidadePopulacional2;
     float pibPerCapita2;
+    float superPoder2;
 
 // Entrada Carta 1
     printf("Digite o Estado da Carta 1 (A-H):  \n");
@@ -32,7 +35,7 @@ int main() {
     scanf(" %[^\n]", Nomecidade1);
 
     printf("Digite a população da cidade: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Digite a área da cidade (km²): ");
     scanf("%f", &area1);
@@ -43,8 +46,10 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
+    //Cálculos da carta 1
     densidadePopulacional1 = populacao1 / area1;
     pibPerCapita1 = (pib1 * 1000000000.0) / populacao1; // PIB per capita em reais
+    superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1.0 / densidadePopulacional1);
 
  // Gera o código automaticamente (Carta 1)
 /*
@@ -69,7 +74,7 @@ int main() {
     scanf(" %[^\n]", Nomecidade2);
 
     printf("Digite a população da cidade: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Digite a área da cidade (km²): ");
     scanf("%f", &area2);
@@ -80,8 +85,10 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+    //Cálculos da carta 2
     densidadePopulacional2 = populacao2 / area2;
     pibPerCapita2 = (pib2 * 1000000000.0) / populacao2; // PIB per capita em reais
+    superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1.0 / densidadePopulacional2); 
 
  // Gera o código automaticamente (Carta 2)
 
@@ -95,7 +102,7 @@ int main() {
     printf("Código: %s\n", codigocarta1);
     printf("Estado: %s\n", Estado1);
     printf("Cidade: %s\n", Nomecidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
@@ -107,12 +114,24 @@ int main() {
     printf("Código: %s\n", codigocarta2);
     printf("Estado: %s\n", Estado2);
     printf("Cidade: %s\n", Nomecidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
     printf("Densidade Populacional: %.2f habitantes/km²\n", densidadePopulacional2);
     printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+
+// Comparar Super Poderes
+    printf ("\nComparação de Super Poderes:\n");
+    printf("Super Poder Carta 1: %.2f\n", superPoder1);
+    printf("Super Poder Carta 2: %.2f\n", superPoder2);
+    printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+    printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
+    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
+    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontosTuristicos1 > pontosTuristicos2);
+    printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidadePopulacional1 < densidadePopulacional2);
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: Carta 1 venceu (%d)\n", superPoder1 > superPoder2);
 
     return 0;
     }
